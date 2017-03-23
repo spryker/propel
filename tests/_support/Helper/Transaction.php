b52b1b0aed288170c4000bf57fe5520332a7236c
René Klatt <rene.klatt@spryker.com>
2017-03-23 15:24:37 +0000

    test

---
+++ tests/_support/Helper/Transaction.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Propel\Helper;
+
+use Codeception\Lib\ModuleContainer;
+use Codeception\Module;
+use Codeception\TestCase;
+use Propel\Runtime\Propel;
+use Silex\Application;
+use Spryker\Zed\Propel\Communication\Plugin\ServiceProvider\PropelServiceProvider;
+
+class Transaction extends Module
+{
+
+    /**
+     * @param \Codeception\Lib\ModuleContainer $moduleContainer
+     * @param null $config
+     */
+    public function __construct(ModuleContainer $moduleContainer, $config = null)
+    {
+        parent::__construct($moduleContainer, $config);
+
+        $propelServiceProvider = new PropelServiceProvider();
+        $propelServiceProvider->boot(new Application());
+    }
+
+    /**
+     * @param \Codeception\TestCase $test
+     *
+     * @return void
+     */
+    public function _before(TestCase $test)
+    {
+        parent::_before($test);
+
+        Propel::getWriteConnection('zed')->beginTransaction();
+    }
+
+    /**
+     * @param \Codeception\TestCase $test
+     *
+     * @return void
+     */
+    public function _after(TestCase $test)
+    {
+        parent::_after($test);
+
+        Propel::getWriteConnection('zed')->rollBack();
+
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            session_destroy();
+        }
+    }
+
+    /**
+     * @param \Codeception\TestCase $test
+     * @param bool $fail
+     *
+     * @return void
+     */
+    public function _failed(TestCase $test, $fail)
+    {
+        parent::_failed($test, $fail);
+
+        Propel::getWriteConnection('zed')->rollBack();
+
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            session_destroy();
+        }
+    }
+}
