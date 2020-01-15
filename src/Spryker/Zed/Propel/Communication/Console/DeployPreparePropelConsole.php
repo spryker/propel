2d4e63cf697929879e2a5aff3acbf34efada23d8
Roberto Filippone <roberto.filippone@spryker.com>
2020-01-15 10:17:39 +0100

    TE-4672: deploy prepare propel console

---
+++ src/Spryker/Zed/Propel/Communication/Console/DeployPreparePropelConsole.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Propel\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Propel\Business\PropelFacadeInterface getFacade()
+ * @method \Spryker\Zed\Propel\Communication\PropelCommunicationFactory getFactory()
+ */
+class DeployPreparePropelConsole extends Console
+{
+    public const COMMAND_NAME = 'propel:deploy:prepare-propel';
+    public const DESCRIPTION = 'Prepares Propel configuration on appserver';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $dependingCommands = [
+            SchemaCopyConsole::COMMAND_NAME,
+            BuildModelConsole::COMMAND_NAME,
+        ];
+
+        foreach ($dependingCommands as $commandName) {
+            $exitCode = $this->runDependingCommand($commandName);
+
+            if ($this->hasError()) {
+                return $exitCode;
+            }
+        }
+
+        return static::CODE_SUCCESS;
+    }
+
+    /**
+     * @param string $command
+     * @param array $arguments
+     *
+     * @return int
+     */
+    protected function runDependingCommand($command, array $arguments = [])
+    {
+        $command = $this->getApplication()->find($command);
+        $arguments['command'] = $command;
+        $input = new ArrayInput($arguments);
+
+        return $command->run($input, $this->output);
+    }
+}
